f74adeb7a84221122853a4e4c095b52868ad374a
Mareska <miriam.kempter@rwth-aachen.de>
2019-11-19 12:25:16 +0100

    Aufgabe 2.2g done

---
+++ MRL_Assignment1_Maze/Assets/Scripts/DoorInteraction.cs
@@ -0,0 +1,34 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour,IInputClickHandler {
+
+    private DoorController door;
+
+    private void Awake()
+    {
+        door = GetComponent<DoorController>();
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (door == null)
+        {
+            Debug.LogError("No DoorController found on the object");
+            return;
+        }
+        door.DoorOpen = !door.DoorOpen;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
