2098690c225ad9d7f3c02aae9bac40270fad63e1
EmilieHK <emilie.hastrup.kiil@rwth-aachen.de>
2019-11-19 11:00:27 +0100

    2.2def done

--- MRL_Assignment1_Maze/Assets/Scripts/MazeMover.cs
+++ MRL_Assignment1_Maze/Assets/Scripts/MazeMover.cs
@@ -3,32 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
-public class MazeMover : MonoBehaviour, IManipulationHandler {
+public class MazeMover : MonoBehaviour, IManipulationHandler
+{
+    private Vector3 startPosition = new Vector3();
+    public float lerpSpeed = 10f;
 
     public void OnManipulationCanceled(ManipulationEventData eventData)
     {
-        throw new System.NotImplementedException();
+        InputManager.Instance.OverrideFocusedObject = null;
     }
 
     public void OnManipulationCompleted(ManipulationEventData eventData)
     {
-        throw new System.NotImplementedException();
+        InputManager.Instance.OverrideFocusedObject = null;
     }
 
     public void OnManipulationStarted(ManipulationEventData eventData)
     {
-        throw new System.NotImplementedException();
+        InputManager.Instance.OverrideFocusedObject = gameObject;
     }
 
     public void OnManipulationUpdated(ManipulationEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Vector3 targetPosition = startPosition + eventData.CumulativeDelta;
+        Vector3 upAxis = targetPosition - transform.parent.position;
+        transform.parent.up = Vector3.Lerp(transform.parent.up, upAxis, lerpSpeed * Time.deltaTime);
     }
+
 
     // Use this for initialization
     void Start () {
-		
-	}
+        startPosition = gameObject.transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
