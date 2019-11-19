4dc48fc92483f7d1dd6a1bd0e451958745236250
Mareska <miriam.kempter@rwth-aachen.de>
2019-11-19 10:29:30 +0100

    Aufgabe 2 a b c

---
+++ MRL_Assignment1_Maze/Assets/Scripts/MazeMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class MazeMover : MonoBehaviour, IManipulationHandler {
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MRL_Assignment1_Maze/Assets/TiltMaze/Scripts/WinningCondition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script which observes if the player has won the game
+/// </summary>
+public class WinningCondition : MonoBehaviour
+{
+    [Tooltip("The prefab for the confetti particle system")]
+    public GameObject confettiPrefab;
+
+    /// <summary>
+    /// Instantiates the confetti particle system in the scene so that it can be accessed
+    /// </summary>
+    private void Start()
+    {
+        GameObject instance = Instantiate(confettiPrefab);
+        transform.parent.GetComponent<GameController>().confettiCanon = instance;
+    }
+
+    /// <summary>
+    /// Called by Unity if an object enters a trigger volume
+    /// Shoots confetti if the marble enters the trigger volume around the flag
+    /// </summary>
+    /// <param name="other">The collider which entered the trigger volume</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // if the marble entered the volume, shoot confetti above the flag
+        if (other.tag == "Marble")
+        {
+            ConfettiManager.Instance.ShootConfetti(transform.position + 0.05f * transform.up);
+        }
+    }
+}
