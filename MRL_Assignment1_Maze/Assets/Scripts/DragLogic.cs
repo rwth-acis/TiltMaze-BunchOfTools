95b783ab30186f735ef1f2059f367af78c00df5c
JulianStaab <julianstaab@web.de>
2019-12-03 20:55:41 +0100

    Implementation complete

---
+++ MRL_Assignment1_Maze/Assets/Scripts/DragLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.UX;
+public class DragLogic : MonoBehaviour {
+
+    private HandDraggable handdrag;
+    private BoundingBox box;
+
+    // Use this for initialization
+    void Awake()
+    {
+        box = GetComponent<BoundingBox>();
+    }
+    void Start () {
+        handdrag = box.Target.GetComponent<HandDraggable>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handdrag.IsDraggingEnabled = box.IsVisible;
+	}
+}
---
+++ MRL_Assignment1_Maze/Assets/SnapLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+public class SnapLogic : MonoBehaviour {
+
+    SolverHandler solHan;
+	// Use this for initialization
+	void Start () {
+        solHan = GetComponent<SolverHandler>();
+	}
+	
+	public void switchMode () {
+        solHan.enabled = !solHan.enabled;
+	}
+}
